a7c3eb425064431c8776fd1a566a622586d2297c
Roelof Rietbroek <r.rietbroek@utwente.nl>
2025-01-11 00:46:37 +0100

    change to pydata theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ author = 'Roelof Rietbroek'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 
-extensions = ['nbsphinx', 'sphinxcontrib.apidoc', 'sphinx.ext.autodoc','sphinx.ext.napoleon','sphinx_github_style']
+extensions = ['nbsphinx', 'sphinxcontrib.apidoc', 'sphinx.ext.autodoc','sphinx.ext.napoleon']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store','**.ipynb_checkpoints']
@@ -33,7 +33,9 @@ apidoc_toc_file=False
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
+html_theme='pydata_sphinx_theme'
+
 html_static_path = ['_static']
 
 napoleon_numpy_docstring = True
@@ -43,5 +45,38 @@ Download this Jupyter notebook from `github <https://github.com/ITC-Water-Resour
 
 ----
 """
+html_theme_options = {
+    "use_edit_page_button": False,
+    "icon_links": [
+        {
+            # Label for this link
+            "name": "GitHub",
+            # URL where the link will redirect
+            "url": "https://github.com/ITC-Water-Resources/shxarray",  # required
+            # Icon class (if "type": "fontawesome"), or path to local image (if "type": "local")
+            "icon": "fa-brands fa-github",
+            # The type of image to be used (see below for details)
+            "type": "fontawesome",
+        }
+   ]
+}
+
+# html_context = {
+    # # "github_url": "https://github.com", # or your GitHub Enterprise site
+    # "github_user": "ITC-Water-Resources",
+    # "github_repo": "shxarray",
+    # "github_version": "main",
+    # "doc_path": "docs/source",
+# }
 
 
+# def linkcode_resolve(domain, info):
+    # if domain != 'py':
+        # return None
+    # if not info['module']:
+        # return None
+    # filename = info['module'].replace('.', '/')
+    # return "https://github.com/ITC-Water-Resources/shxarray/%s.py" % filename
+
+# linkcode_url="https://github.com/ITC-Water-Resources/shxarray"
+# linkcode_link_text="view on"
